2f99de149a14db72bd103e9c1f6b3882335c0be3
Heshan Eranga <erangamx@gmail.com>
2018-04-08 12:50:11 +0530

    commit

---
+++ main.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: heshan
+ *
+ * Created on April 8, 2018, 12:46 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
